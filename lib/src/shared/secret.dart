c14d5bd3282de0c9e772b82016c561a53b0b6b8a
francesc3000 <francesc3000@gmail.com>
2021-07-03 19:41:45 +0200

    no secret
    no secret

--- lib/src/shared/secret.dart
+++
@@ -1,4 +0,0 @@
- final String secret = "35cd14819f8c7ccd368cb91310d5371658d16adb";
- final String clientId = "61262";
-//  final String secret = "b3a29c6dc6394187aec694ca43c8ddd842d91d02";
-//  final String clientId = "60867";
