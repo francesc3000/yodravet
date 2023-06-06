c41f3c1029813b3e669001de0d6d80223b877d5d
francesc3000 <francesc3000@gmail.com>
2023-06-06 23:46:26 +0200

    version final 2023

--- lib/src/shared/edition.dart
+++ lib/src/shared/edition.dart
@@ -1,4 +1,4 @@
 class Edition {
-  // static String get currentEdition => "${DateTime.now().year}DravetTour";
-  static String get currentEdition => "1970DravetTour";
+  static String get currentEdition => "${DateTime.now().year}DravetTour";
+  // static String get currentEdition => "1970DravetTour";
 }
